d621c1fcb584f33924d0773e9651ab8cd7a6cf77
carl_meyrick <carlmeyrickcheng@gmail.com>
2020-10-11 17:55:48 +0800

    Installed materializecss

--- src/App.js
+++ src/App.js
@@ -1,26 +1,57 @@
-import React from 'react';
+import React, {Component} from 'react';
+import { Route, Switch, withRouter, Redirect } from "react-router-dom";
 import logo from './logo.svg';
 import './App.css';
+import 'materialize-css/dist/css/materialize.min.css';
+import M from "materialize-css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  componentDidMount() {
+
+    M.AutoInit();
+  }
+
+  render() {
+    // let routes = (
+    //   <Switch>
+    //     <Route path="/auth" component={asyncAuth} />
+    //     <Route path="/" exact component={BurgerBuilder} />
+    //     <Redirect to="/" />
+    //   </Switch>
+    // );
+
+    // if (this.props.isAuthenticated) {
+    //   routes = (
+    //     <Switch>
+    //       <Route path="/checkout" component={asyncCheckout} />
+    //       <Route path="/orders" component={asyncOrders} />
+    //       <Route path="/logout" component={Logout} />
+    //       <Route path="/auth" component={asyncAuth} />
+    //       <Route path="/" exact component={BurgerBuilder} />
+    //       <Redirect to="/" />
+    //     </Switch>
+    //   );
+    // }
+
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>
+            Edit <code>src/App.js</code> and save to reload.
+          </p>
+          <a
+            className="App-link"
+            href="https://reactjs.org"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            Learn React
+          </a>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
